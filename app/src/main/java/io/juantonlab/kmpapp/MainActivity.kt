a225008301bf2efa8b6a5700e3cab259c427860f
Juan Antonio <1317580+JuantonCodex@users.noreply.github.com>
2024-05-26 17:49:56 -0500

    feat: add borderBottom method to Modifier

--- app/src/main/java/io/juantonlab/kmpapp/MainActivity.kt
+++ app/src/main/java/io/juantonlab/kmpapp/MainActivity.kt
@@ -5,7 +5,6 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
@@ -13,6 +12,7 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
@@ -21,9 +21,12 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -37,24 +40,25 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    val modifier = Modifier
+                    val headerModifier = Modifier
                         .wrapContentSize()
-                        .background(Color.LightGray)
                         .height(50.dp)
+                        .padding(horizontal = 5.dp)
+                        .borderBottom(2.dp, Color.Blue)
                         .clickable { Log.d("kmp", "clicked") }
 
-                    Row(horizontalArrangement = Arrangement.SpaceBetween, verticalAlignment = Alignment.Top) {
+                    Row(
+                        horizontalArrangement = Arrangement.Start,
+                        verticalAlignment = Alignment.Top
+                    ) {
                         Image(
                             painter = painterResource(id = R.drawable.baseline_person_24),
                             contentDescription = "Person",
                             modifier = Modifier
                                 .height(50.dp)
                                 .width(50.dp)
-                                .border(width = 1.dp, color = Color.Blue)
                         )
-                        WelcomeText(modifier, message = "Juan Antonio", fontSize = 15.sp)
-
-
+                        WelcomeHeader(headerModifier, message = "Juan Antonio", fontSize = 15.sp)
                     }
                 }
             }
@@ -63,8 +67,8 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun WelcomeText(modifier: Modifier, message: String, fontSize: TextUnit) {
-    Box(modifier = modifier, contentAlignment = Alignment.Center){
+fun WelcomeHeader(modifier: Modifier, message: String, fontSize: TextUnit) {
+    Box(modifier = modifier, contentAlignment = Alignment.Center) {
         Text(
             text = "Bienvenido, $message",
             color = Color.Blue,
@@ -74,3 +78,17 @@ fun WelcomeText(modifier: Modifier, message: String, fontSize: TextUnit) {
     }
 
 }
+
+
+fun Modifier.borderBottom(borderWidth: Dp, color: Color): Modifier = this.then(
+    Modifier.drawBehind {
+        val strokeWidth = borderWidth.toPx()
+        val y = size.height - strokeWidth / 2
+        drawLine(
+            color = color,
+            start = Offset(0f, y),
+            end = Offset(size.width, y),
+            strokeWidth = strokeWidth
+        )
+    }
+)
