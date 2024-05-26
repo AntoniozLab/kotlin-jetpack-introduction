2319c9da6c4938b13fdd594394808104c720ee49
Juan Antonio <1317580+JuantonCodex@users.noreply.github.com>
2024-05-26 18:59:12 -0500

    feat: rename reusable component and add Card

--- app/src/main/java/io/juantonlab/kmpapp/MainActivity.kt
+++ app/src/main/java/io/juantonlab/kmpapp/MainActivity.kt
@@ -1,20 +1,22 @@
 package io.juantonlab.kmpapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -40,25 +42,69 @@ class MainActivity : ComponentActivity() {
                 Surface(
                     modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background
                 ) {
-                    val headerModifier = Modifier
-                        .wrapContentSize()
-                        .height(50.dp)
-                        .padding(horizontal = 5.dp)
-                        .borderBottom(2.dp, Color.Blue)
-                        .clickable { Log.d("kmp", "clicked") }
-
-                    Row(
-                        horizontalArrangement = Arrangement.Start,
-                        verticalAlignment = Alignment.Top
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(vertical = 15.dp),
+                        contentAlignment = Alignment.TopCenter
                     ) {
-                        Image(
-                            painter = painterResource(id = R.drawable.baseline_person_24),
-                            contentDescription = "Person",
-                            modifier = Modifier
-                                .height(50.dp)
-                                .width(50.dp)
-                        )
-                        WelcomeHeader(headerModifier, message = "Juan Antonio", fontSize = 15.sp)
+                        Card(
+                            modifier = Modifier.width(300.dp),
+                            border = BorderStroke(1.dp, color = Color.Blue),
+                        ) {
+                            Column(
+                                modifier = Modifier.padding(15.dp)
+                            ) {
+                                Row(
+                                    horizontalArrangement = Arrangement.Start,
+                                    verticalAlignment = Alignment.Top,
+                                    modifier = Modifier.fillMaxWidth()
+                                ) {
+
+                                    val headerModifier = Modifier
+                                        .wrapContentSize()
+                                        .height(50.dp)
+                                        .padding(horizontal = 5.dp)
+                                        .borderBottom(2.dp, Color.Blue)
+
+                                    Image(
+                                        painter = painterResource(id = R.drawable.baseline_person_24),
+                                        contentDescription = "Person",
+                                        modifier = Modifier
+                                            .height(50.dp)
+                                            .width(50.dp)
+                                    )
+                                    CustomBox(
+                                        headerModifier,
+                                        message = "Bienvenido, Juan Antonio",
+                                        fontSize = 15.sp
+                                    )
+                                }
+
+                                Row(
+                                    modifier = Modifier.fillMaxWidth(),
+                                    horizontalArrangement = Arrangement.Center,
+                                ) {
+
+
+                                    val boxButtonModifier = Modifier
+                                        .wrapContentSize()
+                                        .height(60.dp)
+                                        .padding(vertical = 15.dp)
+                                        .border(1.dp, color = Color.Black)
+                                        .padding(horizontal = 5.dp)
+
+                                    CustomBox(
+                                        boxButtonModifier,
+                                        message = "Abrir enlace",
+                                        fontSize = 15.sp,
+                                        color = Color.Black
+                                    )
+
+                                }
+
+                            }
+                        }
                     }
                 }
             }
@@ -67,28 +113,22 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun WelcomeHeader(modifier: Modifier, message: String, fontSize: TextUnit) {
+fun CustomBox(
+    modifier: Modifier, message: String, fontSize: TextUnit, color: Color = Color.Blue
+) {
     Box(modifier = modifier, contentAlignment = Alignment.Center) {
         Text(
-            text = "Bienvenido, $message",
-            color = Color.Blue,
-            fontSize = fontSize,
-            fontWeight = FontWeight.Bold
+            text = message, color = color, fontSize = fontSize, fontWeight = FontWeight.Bold
         )
     }
 
 }
 
 
-fun Modifier.borderBottom(borderWidth: Dp, color: Color): Modifier = this.then(
-    Modifier.drawBehind {
-        val strokeWidth = borderWidth.toPx()
-        val y = size.height - strokeWidth / 2
-        drawLine(
-            color = color,
-            start = Offset(0f, y),
-            end = Offset(size.width, y),
-            strokeWidth = strokeWidth
-        )
-    }
-)
+fun Modifier.borderBottom(borderWidth: Dp, color: Color): Modifier = this.then(Modifier.drawBehind {
+    val strokeWidth = borderWidth.toPx()
+    val y = size.height - strokeWidth / 2
+    drawLine(
+        color = color, start = Offset(0f, y), end = Offset(size.width, y), strokeWidth = strokeWidth
+    )
+})
